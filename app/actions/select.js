83b2e2cb7a81387249fb2007cf751135efe7dfb4
Joris Borsboom <jorisborsboom@gmail.com>
2017-10-25 12:46:36 +0530

    Moving updated parameter to ContextOut

--- app/actions/select.js
+++ app/actions/select.js
@@ -5,7 +5,7 @@ class SelectAction{
   op(req, res){
     var selectedNumber =  req.body.result.parameters['selectedNumber'];
     var products = req.body.result.contexts[0].parameters['products'];
-    var selectedProduct = products[selectedNumber - 1].slice(2);
+    var selectedProduct = products[selectedNumber - 1].slice(3);
 
     // Call the search api
     mb.callSearch(selectedProduct).then((output) => {
@@ -15,9 +15,9 @@ class SelectAction{
         //"speech" is the spoken version of the response, "displayText" is the visual version
         //Default response: show added product name
         var response = 'I have added \'' + output.data[0].nm + '\' to your basket(Not really).';
-        var responseJson = stringify({ "speech": response, "displayText": response, "data": {
-           "product":selectedProduct
-        }});
+        var responseJson = stringify({ "speech": response, "displayText": response,
+          "contextOut": [{"name":"orderintent-followup", "lifespan":2, "parameters":{"product": selectedProduct}}]
+        });
       } else {
         //FIXME We do not like this case. The product should be found by the select
         var response = 'There are an unexpected number of results for ' + selectedProduct + '.';
