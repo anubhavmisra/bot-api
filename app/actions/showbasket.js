901ac0722d1e7dfa9278678002da295f7cfc3f3c
Joris Borsboom <jorisborsboom@gmail.com>
2017-11-01 14:53:26 +0530

    Adding event and dummy content to showbasket

--- app/actions/showbasket.js
+++ app/actions/showbasket.js
@@ -10,7 +10,16 @@ class ShowBasketAction{
             //TODO: retrieve basket for this external id
 
             var response = 'This is your basket for tomorrow.';
-            var responseJson = stringify({ "speech": response, "displayText": response});
+            var responseJson = stringify({ "speech": response, "displayText": response, "followupEvent": {
+                "name": "basket_results",
+                "data": {
+                    "items": [
+                        "Item 1",
+                        "Item 2",
+                        "Item 3"
+                    ]
+                }
+            }});
             resolve(responseJson);
         }).catch((error) => {
             console.log(error);
