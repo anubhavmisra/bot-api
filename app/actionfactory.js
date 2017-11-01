73d428aa1efdb4398a594a1c673950d92c6b95ea
Joris Borsboom <jorisborsboom@gmail.com>
2017-11-01 14:58:29 +0530

    Adding a stub to check for external id

--- app/actionfactory.js
+++ app/actionfactory.js
@@ -1,11 +1,13 @@
 var search = require('./actions/search');
 var select = require('./actions/select');
 var showbasket = require('./actions/showbasket');
+var checkexternalid = require('./actions/checkexternalid');
 
 var actionFactory = {
   'search': new search(),
   'select': new select(),
-  'showbasket': new showbasket()
+  'showbasket': new showbasket(),
+  'checkexternalid': new checkexternalid()
 };
 
 module.exports = actionFactory;
---
+++ app/actions/checkexternalid.js
@@ -0,0 +1,23 @@
+var stringify = require('json-stringify');
+var utils = require('../lib/requestutils');
+var mb = require('../lib/milkbasket');
+
+class CheckExtId{
+    op(req, res){
+        return new Promise((resolve, reject) => {
+            
+            var extid = utils.getexternalid(req); 
+
+            //TODO: check extid at MB
+
+            var response = 'Could not check in backend';
+            var responseJson = stringify({ "speech": response, "displayText": response});
+            resolve(responseJson);
+        }).catch((error) => {
+            console.log(error);
+            reject(error);
+        });
+    }
+}
+ 
+module.exports = CheckExtId;
