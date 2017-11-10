8bdf66c40103322539585571acaf035d1ee6bf23
Joris Borsboom <jorisborsboom@gmail.com>
2017-11-10 13:32:08 +0530

    Adding extra logging to help with bot rearrangement

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.post('/api/order', function(req, res) {
   //FIXME: user authentication
   //FIXME: who can access this app? How do we limit this?
 
+  console.log("Receiving request:");
   console.log(JSON.stringify(req.body,null,2));
 
   //switch up logic based on "action" parameter
@@ -22,10 +23,13 @@ app.post('/api/order', function(req, res) {
 
   action.op(req, res).then((responseJson)  => {
     res.setHeader('Content-Type', 'application/json');
+    console.log("Sending response:");
+    console.log(JSON.stringify(req.body,null,2));
     //TODO: maybe better to stringify here?
     res.send(responseJson);
   }).catch((error) => {
     //Let the user know if an error occurred
+    console.log("Error: " + error);
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
   });
