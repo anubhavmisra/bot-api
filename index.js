9b673b64b54b06a13fc3284bf7c7ad635d7d7b70
Joris Borsboom <jorisborsboom@gmail.com>
2017-11-17 15:33:12 +0530

    Check if the user ID is known at MB. Add the MB ID to an output context if it is known.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let bodyParser = require('body-parser');
 
 let actionresolver = require('./app/actionresolver');
 let actionfactory = require('./app/actionfactory');
+let mb = require('./app/lib/milkbasket');
+let utils = require('./app/lib/requestutils');
 
 let app = express();
 app.use(bodyParser.json());       // to support JSON-encoded bodies
@@ -11,28 +13,53 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 app.post('/api/order', function(req, res) {
-  //FIXME: user authentication
-  //FIXME: who can access this app? How do we limit this?
-
   console.log("Receiving request:");
   console.log(JSON.stringify(req.body,null,2));
 
-  //switch up logic based on "action" parameter
-  let actionName = actionresolver.resolveAction(req);
-  let action = actionfactory[actionName];
+  //TODO: check if user is already logged in
+  //Only check id at backend if not found
 
-  action.op(req, res).then((responseJson)  => {
-    res.setHeader('Content-Type', 'application/json');
-    console.log("Sending response:");
-    console.log(JSON.stringify(responseJson,null,2));
-    //TODO: maybe better to stringify here?
-    res.send(responseJson);
+  // user authentication first
+  let extid = utils.getexternalid(req); 
+  
+  //FIXME: behaviour if ext id is empty?
+  mb.isValidUser(extid).then((isValidUser) => {
+    if(!isValidUser){
+      let response = "It seems like you have not regisered your facebook account yet. Please go to " + 
+      "http://www.milkbasket.com/facebook and register your account." ;
+      responseJson = JSON.stringify({ "speech": response, "displayText": response});
+      res.setHeader('Content-Type', 'application/json');
+      res.send(responseJson);
+    } else {
+      let userid = isValidUser.id;
+      //switch up logic based on "action" parameter
+      let actionName = actionresolver.resolveAction(req);
+      let action = actionfactory[actionName];
+
+      action.op(req, res).then((responseJson)  => {
+        parsedJson = JSON.parse(responseJson);
+        //Add the userid to an 'out' context
+        parsedJson.contextOut = [{"name":"usercontext", "lifespan":5, "userid":userid}];
+        
+        console.log("Sending response:");
+        console.log(JSON.stringify(parsedJson,null,2));
+        
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(parsedJson));
+      }).catch((error) => {
+        //Let the user know if an error occurred
+        console.log("Error: " + error);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
+      });
+    }
   }).catch((error) => {
     //Let the user know if an error occurred
     console.log("Error: " + error);
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
   });
+  
 });
 
 app.listen(process.env.PORT || 3001);
