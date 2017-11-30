8183df2f919984ae0f19082a3b1a7ed48f069a01
anubhavmisra <anubhav.misra@empeiriatech.com>
2017-11-30 13:15:21 +0530

    Adding tests

--- test/actions/search.js
+++ test/actions/search.js
@@ -1,21 +1,85 @@
 var assert = require('should');
-var search = require('../../app/actions/search');
+var actionfactory = require('../../app/actionfactory');
+var action = actionfactory['search'];
+var _ = require('lodash');
 describe('search', function () {
   it('should return no search items', function () {
-    //FIXME: Assert that the search results are valid
-    //var searchObj = new search.SearchAction();
-    //searchObj.op(req, res);
-    var results = [];
-    results.length.should.equal(0);
+    var request = {
+      "body": {
+        "result": {
+          "parameters": { "product": "jesus" }
+        }
+      }
+    };
+    var expectedResponse = {
+      "speech": "I could not find any results for jesus.",
+      "displayText": "I could not find any results for jesus."
+    };
+
+
+    action.op(request, expectedResponse).then((responseJson) => {
+      var response = JSON.parse(responseJson);
+      should.exist(response);
+      should(response).has.property('speech');
+      should(response).has.property('displayText');
+      should(expectedResponse.speech).equal(response.speech);
+      should(expectedResponse.displayText).equal(response.displayText);
+    }).catch((error) => {
+      should.not.exist(error);
+    });
   });
   it('should return a single search item', function () {
-    //FIXME: Assert that the search results are valid
-    var results = [1];
-    results.length.should.equal(1);
+    var request = {
+      "body": {
+        "result": {
+          "parameters": { "product": "Harvest Gold 100% Atta Bread" }
+        }
+      }
+    };
+    var expectedResponse = {
+      "speech": "I have added 'Harvest Gold 100% Atta Bread' to your basket(Not really).",
+      "displayText": "I have added 'Harvest Gold 100% Atta Bread' to your basket(Not really)."
+    };
+
+
+    action.op(request, expectedResponse).then((responseJson) => {
+      var response = JSON.parse(responseJson);
+      should.exist(response);
+      should(response).has.property('speech');
+      should(response).has.property('displayText');
+      should(expectedResponse.speech).equal(response.speech);
+      should(expectedResponse.displayText).equal(response.displayText);
+    }).catch((error) => {
+      should.not.exist(error);
+    });
   });
   it('should return multiple search items', function () {
-    //FIXME: Assert that the search results are valid
-    var results = [1, 2];
-    results.length.should.aboveOrEqual(2);
+   should.fail('not defined yet')
+  });
+  it('should return items with multiple brands', function () {
+    should.fail('not defined yet')
+  });
+  it('should return items with multiple quantities', function () {
+    var request = {
+      "body": {
+        "result": {
+          "parameters": { "product": "bread" }
+        }
+      }
+    };
+    action.op(request, null).then((responseJson) => {
+      var response = JSON.parse(responseJson);
+      should.exist(response);
+      should(response).has.property('speech');
+      should(response).has.property('displayText');
+      should(response).has.property('followupEvent');
+      should(response.followupEvent).has.property('name');
+      should(response.followupEvent).has.property('data');
+      should(response.followupEvent.data).has.property('quantities');
+      should(response.followupEvent.data).has.property('moreresults');
+      should(response.followupEvent.data).has.property('morequantities');
+    }).catch((error) => {
+      should.not.exist(error);
+    });
   });
 });
--- test/actions/select.js
+++ test/actions/select.js
@@ -1,8 +1,6 @@
 var assert = require('should');
 describe('select', function () {
   it('should add item to the basket', function () {
-    //FIXME: Assert that the search results are valid
-    var basketItems = [1];
-    basketItems.length.should.aboveOrEqual(1);
+    should.fail('not defined yet');
   });
 });
